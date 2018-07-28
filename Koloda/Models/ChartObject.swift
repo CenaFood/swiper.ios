0611711132789f854ab9f1fdbacea087109054c9
Thibault Gagnaux <thibault.gagnaux@students.fhnw.ch>
2018-07-28 18:29:17 +0200

    Adds summary swipe screen draft

---
+++ Koloda/Models/ChartObject.swift
@@ -0,0 +1,19 @@
+//
+//  ChartObject.swift
+//  cena
+//
+//  Created by Thibault Gagnaux on 25.07.18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ChartObject {
+    
+    var color: UIColor
+    var percentage: Int
+    var title: String
+    var description: String
+    var logoImage: UIImage
+}
